141e69b0fafe9c1ad49be74a2702b7dac2530352
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-23 11:17:36 +0700

    #17 - Register API Routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@login');
+
+Route::middleware('auth:api')->group(function () {
+    Route::resource('books', 'BookController');
+});
