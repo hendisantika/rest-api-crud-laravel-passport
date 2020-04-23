bf959bab18b02c9842016f73545b1825b92b82e9
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-23 11:11:49 +0700

    #8 - Add User Login function in Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,4 +43,26 @@ class UserController extends Controller
         ];
         return response()->json($response, 200);
     }
+
+    /**
+     * User Login
+     */
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            // return response
+            $response = [
+                'success' => true,
+                'message' => 'User login successful',
+            ];
+            return response()->json($response, 200);
+        } else {
+            // return response
+            $response = [
+                'success' => false,
+                'message' => 'Unauthorised',
+            ];
+            return response()->json($response, 404);
+        }
+    }
 }
